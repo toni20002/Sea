001ba8ec63228d146ce4f17ac7fb093e86dc8935
Toni Hristov <toni20002@abv.bg>
2021-01-25 20:52:05 +0200

    Added logo and navbar resizing functionality

--- functions.js
+++ functions.js
@@ -16,9 +16,11 @@ let blackLogo = "Logos/blackSea.png";
 
 $(document).ready(function () {
     $(".navbar").on( 'mouseenter mouseleave', function () {
-        if ($(".navbar-brand img").attr("src") == whiteLogo) {
+
+     if ($(".navbar-brand img").attr("src") == whiteLogo) {
             $(".navbar-brand img").attr("src", blackLogo);
         }
+
         else{
             $(".navbar-brand img").attr("src", whiteLogo);
         }
@@ -31,14 +33,20 @@ $(document).ready(function() {
     });
 })
 
-//navbar color change on hover
+//navbar color change on hover, also added functionality to change the logo with smaller one
 $(document).ready(function() {
     $(".navbar").on({
         mouseenter: (function() {
             $(".navbar").css("background-color", "white");
+            if ($("#nav").innerWidth() < 500) {
+                $(".navbar-brand img").attr("src", smLogoB);
+            }
         }),
         mouseleave: (function() {
         $(".navbar").css("background-color", "black");
+            if ($("#nav").innerWidth() < 500) {
+                $(".navbar-brand img").attr("src", smLogoW);
+            }
         })
     });})
 
@@ -48,6 +56,8 @@ let smSlider1 = "SliderPhone/slider1.JPG";
 let smSlider2 = "SliderPhone/slider2.JPG";
 let smSlider3 = "SliderPhone/slider3.JPG";
 let smCreator = "SliderPhone/abiSmall.JPG";
+let smLogoW = "Logos/smallLogoWhite.png";
+let smLogoB = "Logos/smallLogoBlack.png";
 
 $(document).ready(function () {
     let currentWidth = window.innerWidth;
@@ -59,7 +69,8 @@ $(document).ready(function () {
         $("#slider2").attr("src", smSlider2);
         $("#slider3").attr("src", smSlider3);
         $("#creator").attr("src", smCreator);
-        $("#nav").height("55px");
+        $("#nav").height("45px");
+        $(".navbar-brand img").attr("src", smLogoW); //this one is being set as default if the page has width < 500px
       //  console.log($("#nav").height());
     }
 })
